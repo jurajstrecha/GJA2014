57f575d579a9e9c9b192639614b890c966c5af05
NoxArt <petruzelka@nox-art.cz>
2014-03-29 19:29:11 +0100

    Added image attaching, camera intent, tweet state preservation

--- src/cz/fit/gja/twitter/TweetActivity.java
+++ src/cz/fit/gja/twitter/TweetActivity.java
@@ -1,20 +1,33 @@
 package cz.fit.gja.twitter;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import cz.fit.gja.twitter.dialogs.ThumbnailDialog;
 
 public class TweetActivity extends LoggedActivity
 {
+	static final int THUMBNAIL_SIZE = 160;
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	
+	String tweet = "";
+	EditText textarea;
+	ImageView thumbnail;
+	Bitmap attachedImage;
 	
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -22,11 +35,42 @@ public class TweetActivity extends LoggedActivity
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.new_tweet);
 		
-		setTitle(R.string.title_tweet);
+		if( savedInstanceState != null ) {
+			attachedImage = savedInstanceState.getParcelable("image");
+			tweet = savedInstanceState.getString("tweet");
+		}
 		
+		setTitle(R.string.title_tweet);
+		initializeForm();
+	}
+	
+	private void initializeForm() {
 		View content = (View)findViewById(android.R.id.content);
 		if( content != null ) {
-			final EditText textarea = (EditText)content.findViewById(R.id.tweet);
+			
+			// Thumbnail
+			thumbnail = (ImageView)content.findViewById(R.id.thumbnail);
+			if( attachedImage != null ) {
+				setThumbnail(attachedImage);
+			}
+			
+			thumbnail.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					ThumbnailDialog dialog = new ThumbnailDialog();
+					dialog.show(getFragmentManager(), "thumbnail");
+				}
+			});
+			
+			// Buttons
+			ImageButton camera = (ImageButton)content.findViewById(R.id.camera);
+			camera.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					buttonCamera();
+				}
+			});
+			
+			// Limit
+			textarea = (EditText)content.findViewById(R.id.tweet);
 			final TextView limit = (TextView)content.findViewById(R.id.characters_remaining);
 			final int textColor = limit.getTextColors().getDefaultColor();
 			
@@ -46,8 +90,60 @@ public class TweetActivity extends LoggedActivity
 					}
 				});
 			}
+			
+			textarea.setText(tweet);
+			
 		}
     }
+	
+	private void buttonCamera() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && thumbnail != null) {
+			Bundle extras = data.getExtras();
+			attachedImage = (Bitmap) extras.get("data");
+			setThumbnail(attachedImage);
+		}
+	}
+	
+	private void setThumbnail(Bitmap imageBitmap) {
+		if( thumbnail == null ) {
+			return;
+		}
+		
+		if( imageBitmap != null ) {
+			thumbnail.setImageBitmap(imageBitmap);
+			if( thumbnail.getLayoutParams().width < thumbnail.getLayoutParams().height ) {
+				thumbnail.getLayoutParams().width = THUMBNAIL_SIZE;
+				thumbnail.getLayoutParams().height = (int)((float)imageBitmap.getHeight() * (THUMBNAIL_SIZE / (float)imageBitmap.getWidth()));
+			} else {
+				thumbnail.getLayoutParams().width = (int)((float)imageBitmap.getWidth()* (THUMBNAIL_SIZE / (float)imageBitmap.getHeight()));
+				thumbnail.getLayoutParams().height = THUMBNAIL_SIZE;
+			}
+		} else {
+			thumbnail.setImageBitmap(null);
+			thumbnail.getLayoutParams().width = 0;
+			thumbnail.getLayoutParams().height = 0;
+		}
+	}
+	
+	public void clearImage() {
+		attachedImage = null;
+		setThumbnail(null);
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle icicle) {
+		super.onSaveInstanceState(icicle);
+		icicle.putString("tweet", textarea.getText().toString());
+		icicle.putParcelable("image", attachedImage);
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
---
+++ src/cz/fit/gja/twitter/dialogs/ThumbnailDialog.java
@@ -0,0 +1,42 @@
+/**
+ * Worked hours tracking application
+ * 
+ * @author Jiří "NoxArt" Petruželka
+ * @copyright Jiří Petruželka 2013
+ */
+package cz.fit.gja.twitter.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import cz.fit.gja.twitter.R;
+import cz.fit.gja.twitter.TweetActivity;
+
+/**
+ * Fragment for picking a date
+ * 
+ * @author Nox
+ */
+public class ThumbnailDialog extends DialogFragment {
+		
+    @Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		final TweetActivity activity = (TweetActivity)getActivity();
+		
+		String[] actions = new String[1];
+		actions[0] = getString(R.string.tweet_thumbnail_clear);
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setTitle( getString(R.string.tweet_thumbnail_dialog) )
+			.setItems(actions, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					activity.clearImage();
+				}
+			});
+			   
+		return builder.create();
+	}
+	
+}
