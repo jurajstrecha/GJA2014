792e0b707ba668b369a2676f383fee34ca8f5ac7
NoxArt <petruzelka@nox-art.cz>
2014-03-29 18:35:52 +0100

    Implemented 140 limit gui

--- src/cz/fit/gja/twitter/TweetActivity.java
+++ src/cz/fit/gja/twitter/TweetActivity.java
@@ -1,10 +1,16 @@
 package cz.fit.gja.twitter;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TweetActivity extends LoggedActivity
@@ -17,6 +23,30 @@ public class TweetActivity extends LoggedActivity
         setContentView(R.layout.new_tweet);
 		
 		setTitle(R.string.title_tweet);
+		
+		View content = (View)findViewById(android.R.id.content);
+		if( content != null ) {
+			final EditText textarea = (EditText)content.findViewById(R.id.tweet);
+			final TextView limit = (TextView)content.findViewById(R.id.characters_remaining);
+			final int textColor = limit.getTextColors().getDefaultColor();
+			
+			if( textarea != null && limit != null ) {
+				textarea.addTextChangedListener(new TextWatcher() {
+					public void beforeTextChanged(CharSequence cs, int i, int i1, int i2) {}
+					public void onTextChanged(CharSequence cs, int i, int i1, int i2) {}
+					public void afterTextChanged(Editable edtbl) {
+						Integer remaining = 140 - edtbl.length();
+						limit.setText(remaining.toString());
+						
+						if( remaining < 0 ) {
+							limit.setTextColor(Color.rgb(220, 0, 20));
+						} else {
+							limit.setTextColor(textColor);
+						}
+					}
+				});
+			}
+		}
     }
 	
 	@Override
